dcbe96418c3d8c55aae6f5a15027c7006fa67b41
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-04-22 18:49:49 -0300

    feat(migration): tabela usuarios

--- Backend/src/database/migrations/20250422175417-usuarios.js
+++ Backend/src/database/migrations/20250422175417-usuarios.js
@@ -2,21 +2,55 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'users', {
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+      id: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      cpf: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        validate: {
+          isIn: [['paciente', 'médico', 'admin']]
+        }
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+    }
+    }
+    );
+},
+
+  async down(queryInterface, Sequelize) {
+  /**
+   * Add reverting commands here.
+   *
+   * Example:
+   * await queryInterface.dropTable('users');
+   */
+}
 };
