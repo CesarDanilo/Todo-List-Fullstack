618041bd31c49d056b08c1835f89a68e52f7bcc4
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-04-23 14:51:08 -0300

    feat(models): models usuario

---
+++ Backend/src/database/models/Usuarios.js
@@ -0,0 +1,54 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Usuarios extends Model {
+        static associate(models) {
+            // Users.hasMany(models.Pedido, { foreignKey: 'usuario_id' });
+        }
+    }
+    Usuarios.init({
+        id: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        cpf: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        role: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            validate: {
+                isIn: [['paciente', 'médico', 'admin']]
+            }
+        },
+        createdAt: {
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            type: DataTypes.DATE
+        }
+    }, {
+        sequelize,
+        modelName: 'Usuarios',
+        tableName: 'usuarios',
+        timestamps: true // Habilita o gerenciamento automático de createdAt e updatedAt
+    });
+
+    return Usuarios;
+};
