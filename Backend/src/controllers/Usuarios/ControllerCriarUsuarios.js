a86f226686be5e5f881404e51ef150759244787f
CSR <billypalacios616@gmail.com>
2025-04-24 14:13:02 -0300

    feat(controller)!: criando usuario

--- Backend/src/controllers/Usuarios/ControllerCriarUsuarios.js
+++ Backend/src/controllers/Usuarios/ControllerCriarUsuarios.js
@@ -4,12 +4,28 @@ const { v4: uuidv4 } = require('uuid');
 const ControllerCriarUsuario = async (req, res) => {
     try {
         const data = req.body;
+
+        // Validando informações recebidas
         if (!data.name || !data.email || !data.password || !data.status) {
             return res.status(400).json({
                 msg: "Não foi possivel gravar! Todos os campos são obrigatorios!",
                 data: req.body
             })
         }
+
+        const userId = uuidv4();
+        data.id = userId;
+
+        let result;
+        try {
+            result = await Usuarios.create(data);
+        } catch (error) {
+            return res.status(400).json({
+                msg: "Não foi possível gravar!",
+                erro: error.message
+            })
+        }
+
     } catch (error) {
 
     }
