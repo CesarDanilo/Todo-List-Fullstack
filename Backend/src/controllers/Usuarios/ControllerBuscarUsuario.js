f73151cf0eb251b21b3f48ae6918207d0846c49e
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-04-25 14:20:23 -0300

    feat(controller): buscar usuario

---
+++ Backend/src/controllers/Usuarios/ControllerBuscarUsuario.js
@@ -0,0 +1,7 @@
+const ControllerBuscarUsuarios = async (req, res, next) => {
+    return res.status(200).json({
+        msg: "Rota funcionando!"
+    })
+}
+
+export default ControllerBuscarUsuarios;
