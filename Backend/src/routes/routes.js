eca30c8018135c2633029c0de062105c5d7ec947
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-04-25 14:40:08 -0300

    ajustando rotas

--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const routes = express.Router();
 const user_api = require("../api/usuarios/api-usuarios");
 
-routes.use('/user', user_api);
+routes.use('/usuarios', user_api);
 
 module.exports = routes;
--- Backend/src/server/index.js
+++ Backend/src/server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 4444;
+const port = 3001;
 const routes = require('../routes/routes')
 
 app.use(routes);
+app.get('/', (req, res) => {
+    return res.status(200)
+})
 
 app.listen(port, () => {
     console.log(`SERVIDOR RODANDO: http:/localhost:${port}`);
