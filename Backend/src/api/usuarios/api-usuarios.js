49317dd4eb186c47615c6ee15c1aa1c3b9216737
Cesar <billypalacios616@gmail.com>
2025-04-27 15:00:50 -0300

    mudando nome de rota (auth/register)

--- Backend/src/api/usuarios/api-usuarios.js
+++ Backend/src/api/usuarios/api-usuarios.js
@@ -4,7 +4,7 @@ const userController = require('../../controllers/Usuarios');
 
 const createUserBcrypt = require('../../middleware/createUserBcrypt');
 
-routes.post('/create-user', createUserBcrypt , userController.criarUsuario);
+routes.post('/auth/register', createUserBcrypt , userController.criarUsuario);
 routes.get('/', userController.buscarUsuario)
 
 module.exports = routes;
