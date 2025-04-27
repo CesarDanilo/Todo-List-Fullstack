bf332633268c664c28976bdefa1d112a028732ef
Cesar <billypalacios616@gmail.com>
2025-04-27 14:56:22 -0300

    feat(get users): buscando dados

--- Backend/src/controllers/Usuarios/ControllerBuscarUsuario.js
+++ Backend/src/controllers/Usuarios/ControllerBuscarUsuario.js
@@ -1,7 +1,30 @@
+const { Usuarios } = require('../../database/models');
+
 const ControllerBuscarUsuarios = async (req, res, next) => {
-    return res.status(200).json({
-        msg: "Rota funcionando!"
-    })
+
+    const { id } = req.query;
+
+    try {
+        let whare = {};
+
+        if (id) {
+            whare.id = id;
+        }
+
+        const result = await Usuarios.findAll({
+            whare,
+            attributes: ['id', 'username', 'email']
+        })
+
+        return res.status(200).json({
+            data: result
+        });
+
+    } catch (error) {
+        const msg = 'Erro ao tentar listar Users';
+        console.error(error);
+        return res.status(400).json({ msg, erro: error.message });
+    }
 }
 
 module.exports = ControllerBuscarUsuarios;
