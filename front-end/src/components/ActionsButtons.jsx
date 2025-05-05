07ac97038159dbc680192950a5863034768626af
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-05-05 14:38:56 -0300

    criando actions

---
+++ front-end/src/components/ActionsButtons.jsx
@@ -0,0 +1,5 @@
+export default function ActionsButtons({ type }) {
+    return (
+        <div></div>
+    )
+}
--- front-end/src/components/ButtonNew.jsx
+++ front-end/src/components/ButtonNew.jsx
@@ -5,7 +5,7 @@ export default function ButtonNew() {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
-            <p>NEW</p>
+            <p>Add</p>
         </button>
     )
 }
