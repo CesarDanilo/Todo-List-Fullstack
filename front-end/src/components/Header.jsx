76e5d091d3916fe0c66cc30c44d3f546d642d150
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-05-05 14:34:18 -0300

    alinhando componentes

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -2,7 +2,7 @@ import ButtonNew from "./ButtonNew"
 
 export default function Header({ title }) {
     return (
-        <div className="p-4">
+        <div className="flex p-4 w-full justify-between ">
             <h1 className="text-lg">{title}</h1>
             <ButtonNew />
         </div>
