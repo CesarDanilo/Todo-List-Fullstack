039c46f4e1b50b89329e2a34ccb077ffa513db3f
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-05-05 14:30:19 -0300

    create button

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,7 +1,10 @@
+import ButtonNew from "./ButtonNew"
+
 export default function Header({ title }) {
     return (
         <div className="p-4">
             <h1 className="text-lg">{title}</h1>
+            <ButtonNew />
         </div>
     )
 }
