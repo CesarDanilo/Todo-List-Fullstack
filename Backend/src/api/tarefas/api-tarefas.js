d4a6bae8fd131e1fb4cc3bcdaf7353e6cc842386
CSR <billypalacios616@gmail.com>
2025-05-06 12:05:28 -0300

    feat: adiciona o arquivo ControllerDeletarTarefa.js que deleta as tarefas por id

--- Backend/src/api/tarefas/api-tarefas.js
+++ Backend/src/api/tarefas/api-tarefas.js
@@ -2,7 +2,9 @@ const express = require('express');
 const routes = express.Router();
 const ControllerCriarTarefa = require('../../controllers/Tarefas');
 const ControllerBuscarTarefas = require('../../controllers/Tarefas');
+const ControllerDeletarTarefas = require('../../controllers/Tarefas');
 
 routes.post('/create-task', ControllerCriarTarefa.criarTarefa);
 routes.get('/', ControllerBuscarTarefas.buscarTarefa);
+routes.delete('/:id', ControllerDeletarTarefas.deletarTarefa);
 module.exports = routes;
