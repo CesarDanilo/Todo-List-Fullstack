6aa785a10e7349a362236851c03f5120560239ff
CSR <billypalacios616@gmail.com>
2025-05-12 12:53:02 -0300

    feat: implementando panding

--- front-end/src/pages/PendingTasks.jsx
+++ front-end/src/pages/PendingTasks.jsx
@@ -1,8 +1,132 @@
+import axios from 'axios';
+import { useState, useEffect, useContext } from 'react';
+import Header from '../components/Header';
+import ActionsButtons from '../components/ActionsButtons';
+import { deleteTasks } from '../functions/deleteTask';
+import DialogForm from '../components/DialogForm';
+import DialogDelete from '../components/DialogDelete';
+import { getTaskForId } from '../functions/getTaskForId';
+import { contextNumberTasks } from '../context/total_number_of_tasks';
+
 export default function PendingTasks() {
-    return (
-      <div>
-        <h1>PendingTasks</h1>
-      </div>
-    );
-  }
-  
+  const [tarefas, setTarefas] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [dados, setDados] = useState([]);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [tarefaParaDeletar, setTarefaParaDeletar] = useState(null);
+
+  const { setPendingTarefasLength } = useContext(contextNumberTasks);
+
+  const fetchTarefas = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/tarefas/?task_status=false');
+
+      if (response.data.data && Array.isArray(response.data.data)) {
+        setTarefas(response.data.data);
+        setPendingTarefasLength(response.data.data.length);
+      } else {
+        throw new Error("Formato de dados inválido");
+      }
+
+    } catch (error) {
+      console.error("Erro ao buscar tarefas:", error);
+      setError(error.message || "Erro ao carregar tarefas");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDeleteTask = async (id) => {
+    try {
+      await deleteTasks(id);
+      fetchTarefas();
+    } catch (error) {
+      console.error("Erro ao deletar tarefa:", error);
+    }
+  };
+
+  const handleUpdateTask = async (id) => {
+    try {
+      const tarefa = await getTaskForId(id);
+      setDados(tarefa);
+      setIsOpen(true);
+    } catch (error) {
+      console.log("Erro no handleUpdateTask:", error);
+    }
+  };
+
+  const handleClickDelete = (tarefa) => {
+    setTarefaParaDeletar(tarefa);
+    setShowDeleteDialog(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    if (!tarefaParaDeletar) return;
+    await handleDeleteTask(tarefaParaDeletar.id);
+    setShowDeleteDialog(false);
+    setTarefaParaDeletar(null);
+  };
+
+  const handleCancelDelete = () => {
+    setShowDeleteDialog(false);
+    setTarefaParaDeletar(null);
+  };
+
+  useEffect(() => {
+    fetchTarefas();
+  }, []);
+
+  if (loading) return <div className="p-4">Carregando tarefas...</div>;
+  if (error) return <div className="p-4 text-red-500">Erro: {error}</div>;
+
+  return (
+    <div className="p-4">
+
+      <Header title={'PENDING'} fetchTarefas={fetchTarefas} />
+
+      {isOpen && (
+        <DialogForm setIsOpen={setIsOpen} fetchTarefas={fetchTarefas} dados={dados} />
+      )}
+
+      {showDeleteDialog && (
+        <DialogDelete onConfirm={handleConfirmDelete} onCancel={handleCancelDelete} />
+      )}
+
+      <table className="min-w-full rounded-lg overflow-hidden shadow-sm">
+        <thead>
+          <tr className="bg-[#f1f1eb]">
+            <th className="px-6 py-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200">Title</th>
+            <th className="px-6 py-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200">Descrição</th>
+            <th className="px-6 py-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200">Status</th>
+            <th className="px-6 py-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200">Data & Hora</th>
+            <th className="px-6 py-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200">Actions</th>
+          </tr>
+        </thead>
+
+        <tbody className="bg-white divide-y divide-gray-100">
+          {tarefas.map((tarefa) => (
+            <tr key={tarefa.id} className="hover:bg-[#f9f9f5] transition-colors duration-150">
+              <td className="px-6 py-4 text-sm font-medium text-gray-900">{tarefa.title || 'Sem título'}</td>
+              <td className="px-6 py-4 text-sm text-gray-600">{tarefa.task_description || 'Sem descrição'}</td>
+              <td className={`px-6 py-4 text-sm ${tarefa.task_status ? "text-green-500" : "text-red-700"}`}>
+                {tarefa.task_status ? 'active' : 'disabled'}
+              </td>
+              <td className="px-6 py-4 text-sm text-gray-600">
+                {/* {tarefa.data ? new Date(tarefa.data).toLocaleString() : 'Sem data'} */}
+                {tarefa.data
+                  ? new Date(new Date(tarefa.data).getTime() + 3 * 60 * 60 * 1000).toLocaleString()
+                  : 'Sem data'}
+              </td>
+              <td className="flex px-6 py-4 text-sm gap-1 text-gray-600">
+                <ActionsButtons type="Edit" onClick={() => handleUpdateTask(tarefa.id)} />
+                <ActionsButtons type="Delete" onClick={() => handleClickDelete(tarefa)} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
