3049de484afc2538e230f7209d3943750cbb0695
CSR <billypalacios616@gmail.com>
2025-05-13 15:56:36 -0300

    feat: create route api

--- Backend/src/api/usuarios/api-usuarios.js
+++ Backend/src/api/usuarios/api-usuarios.js
@@ -3,8 +3,10 @@ const routes = express.Router();
 const userController = require('../../controllers/Usuarios');
 
 const createUserBcrypt = require('../../middleware/createUserBcrypt');
+const validationUserLogin = require('../../middleware/validationUserLogin')
 
-routes.post('/auth/register', createUserBcrypt , userController.criarUsuario);
+routes.post('/auth/register', createUserBcrypt, userController.criarUsuario);
+routes.post('/auth/login', validationUserLogin);
 routes.get('/', userController.buscarUsuario)
 
 module.exports = routes;
