181116610349823df071b87cf7239f0e9df160ce
CSR <billypalacios616@gmail.com>
2025-05-16 14:08:03 -0300

    chore: refatorando buttonNew.jsx

--- front-end/src/components/ButtonNew.jsx
+++ front-end/src/components/ButtonNew.jsx
@@ -1,11 +1,21 @@
-
 export default function ButtonNew({ setIsOpen }) {
     return (
-        <button type="submit" onClick={() => setIsOpen(true)} className="flex w-20 h-auto p-2 rounded-md bg-black text-white cursor-pointer gap-1 hover:bg-gray-800">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
+        <button
+            type="button"
+            onClick={() => setIsOpen(true)}
+            className="flex items-center gap-1 px-3 py-2 rounded bg-black text-white hover:bg-gray-800 transition-colors duration-150 cursor-pointer"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+            >
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
-            <p>Add</p>
+            <span>Add</span>
         </button>
-    )
+    );
 }
