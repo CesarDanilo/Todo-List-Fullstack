2b78008f3a36c23b6dd24b263923cd486c1cfa21
CSR <billypalacios616@gmail.com>
2025-05-20 09:18:55 -0300

    feat: configurando config.js v-1

--- Backend/src/database/config/config.js
+++ Backend/src/database/config/config.js
@@ -1,28 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres'
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres'
-  },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.PGUSER || process.env.DB_USER,
+    password: process.env.PGPASSWORD || process.env.DB_PASSWORD,
+    database: process.env.PGDATABASE || process.env.DB_NAME,
+    host: process.env.PGHOST || process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres'
   }
 };
